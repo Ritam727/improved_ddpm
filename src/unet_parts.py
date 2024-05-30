40426708b71c3476d463a8b7edb7cc24a551444a
Ritam727 <b20127@students.iitmandi.ac.in>
2024-05-30 06:46:42 +0000

    Added some more unet parts

--- src/unet_parts.py
+++ src/unet_parts.py
@@ -32,7 +32,8 @@ class TimeEmbedding(nn.Module):
         """
         
         return self.temb(t)
-    
+
+
 class ResidualBlock(nn.Module):
     
     def __init__(self, in_channels : int, out_channels : int, d_time : int):
@@ -74,3 +75,64 @@ class ResidualBlock(nn.Module):
         x = self.block2(x)
         
         return x + self.residual_layer(residue)
+
+
+class DownSample(nn.Module):
+    
+    def __init__(self, in_channels : int, out_channels : int):
+        """
+            Halves the dimensions of the input image using a convolutional layer
+        """
+        
+        super().__init__()
+        self.layer = nn.Sequential(
+            nn.GroupNorm(32, in_channels),
+            nn.SiLU(),
+            nn.Conv2d(in_channels, out_channels, kernel_size = 3, stride = 2, padding = 1)
+        )
+        
+    def forward(self, x : torch.Tensor) -> torch.Tensor:
+        """
+            Input Shape : (B, C_in, H, W)
+            Output Shape : (B, C_out, H // 2, W // 2)
+        """
+        
+        return self.layer(x)
+
+
+class UpSample(nn.Module):
+    
+    def __init__(self, in_channels : int, out_channels : int):
+        """
+            Doubles the dimensions of input image and then passes through a convolutional layer
+        """
+        
+        super().__init__()
+        self.layer = nn.Sequential(
+            nn.Upsample(scale_factor = 2),
+            nn.Conv2d(in_channels, out_channels, kernel_size = 1, stride = 1, padding = 0)
+        )
+        
+    def forward(self, x : torch.Tensor) -> torch.Tensor:
+        """
+            Input Shape : (B, C_in, H, W),
+            Output Shape : (B, C_out, H * , W * 2)
+        """
+        
+        return self.layer(x)
+
+
+class SwitchSequential(nn.Sequential):
+    
+    def forward(self, x : torch.Tensor, t : torch.Tensor) -> torch.Tensor:
+        """
+            Input Shape : (B, C_in, H, W), (B, D)
+            Output Shape : (B, C_out, H, W)
+        """
+        
+        for layer in self:
+            if isinstance(layer, ResidualBlock):
+                x = layer(x, t)
+            else:
+                x = layer(x)
+        return x
